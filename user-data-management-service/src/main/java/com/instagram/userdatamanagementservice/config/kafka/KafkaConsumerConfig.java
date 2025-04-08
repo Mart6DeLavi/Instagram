bb6aa1cc0c8ccc1c25e668e5d99039ea6165e15f
martix_de_lavi <azmater59034@gmail.com>
2025-04-08 22:43:40 +0200

    Fixed bug in Consumer Configuration

--- user-data-management-service/src/main/java/com/instagram/userdatamanagementservice/config/kafka/KafkaConsumerConfig.java
+++ user-data-management-service/src/main/java/com/instagram/userdatamanagementservice/config/kafka/KafkaConsumerConfig.java
@@ -8,31 +8,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaConsumerConfig<T> {
+public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
     @Bean
-    public ConsumerFactory<String, T> consumerFactory() {
+    public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
+
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.instagram.userdatamanagementservice.dto.kafka.UserRegistrationDto");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+
         return new DefaultKafkaConsumerFactory<>(props,
                 new StringDeserializer(),
-                new JsonDeserializer<>());
+                new ErrorHandlingDeserializer<>(new JsonDeserializer<>(Object.class)));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, T> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
--- user-data-management-service/src/main/java/com/instagram/userdatamanagementservice/kafka/KafkaConsumer.java
+++ user-data-management-service/src/main/java/com/instagram/userdatamanagementservice/kafka/KafkaConsumer.java
@@ -3,12 +3,14 @@ package com.instagram.userdatamanagementservice.kafka;
 import com.instagram.userdatamanagementservice.dto.kafka.UserAuthenticationDto;
 import com.instagram.userdatamanagementservice.dto.kafka.UserRegistrationDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KafkaConsumer {
@@ -18,6 +20,7 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "${topics.authentication-service.user-management-service.consumer.registration}", groupId = "authentication-service")
     public void consumeNewUser(UserRegistrationDto userRegistrationDto) {
+        log.info("Received new user {}", userRegistrationDto);
         registrationQueue.offer(userRegistrationDto);
     }
 
