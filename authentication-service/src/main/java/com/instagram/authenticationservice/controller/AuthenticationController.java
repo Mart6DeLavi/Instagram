df9ac6468e72927499e5a540166928237b1b61ff
martix_de_lavi <azmater59034@gmail.com>
2025-04-10 13:42:47 +0200

    Changed bug in authentication controller

--- authentication-service/src/main/java/com/instagram/authenticationservice/controller/AuthenticationController.java
+++ authentication-service/src/main/java/com/instagram/authenticationservice/controller/AuthenticationController.java
@@ -4,10 +4,13 @@ import com.instagram.dto.kafka.UserAuthenticationDto;
 import com.instagram.dto.kafka.UserRegistrationDto;
 import com.instagram.authenticationservice.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -16,9 +19,10 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public String registerNewUser(UserRegistrationDto userRegistrationDto) {
+    public String registerNewUser(@RequestBody UserRegistrationDto userRegistrationDto) {
         try {
             authenticationService.registerNewUser(userRegistrationDto);
+            log.info("Registered new user: {}", userRegistrationDto);
             return "✅ User registered successfully";
         } catch (Exception ex) {
             return "❌ User registration failed. Message: " + ex.getMessage();
