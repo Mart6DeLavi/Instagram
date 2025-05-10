620a5931f010991590b8bcc5e1c27e922af554b7
martix_de_lavi <azmater59034@gmail.com>
2025-05-11 01:09:39 +0200

    Change .gitignore file and added caching where redis is connected

--- security-service/src/main/java/com/instagram/authenticationservice/SecurityServiceApplication.java
+++ security-service/src/main/java/com/instagram/authenticationservice/SecurityServiceApplication.java
@@ -2,12 +2,14 @@ package com.instagram.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.instagram")
 @EnableDiscoveryClient
+@EnableCaching
 public class SecurityServiceApplication {
 
     public static void main(String[] args) {
